9d65fec0db24e7847d712e5ee692acdcf5da78dc
KaushalTopiya30 <132578818+KaushalTopiya30@users.noreply.github.com>
2023-05-04 18:47:51 +0530

    Add files via upload

---
+++ k.py
@@ -0,0 +1,269 @@
+a=("----------------Breakfast Menu----------------")
+
+
+b=("select_Opt.       Dishes                 Price")
+c=("                                            ")
+
+d=("      1=Gathiya Jalebi   -----      70.00 ")
+
+e=("      2=Masala Dosa      -----      30.00 ")
+
+f=("      3=Plain Dosa       -----      20.00 ")
+g=("      4=Papper Dosa      -----      20.00 ")
+h=("      5=Edali Sanbhar    -----      40.00 ")
+ins=("      6=Tea              -----      25.00 ")
+j=("      7=Coffee           -----      45.00 ")
+kin=("                                          ")
+l=("-----------Luncheon and Dinner Menu------------")
+m=("                                          ")
+n=("     8=Gujrati Thali    -----      120.00 ")
+o=("     9=Bhajiya          -----       80.00 ")
+p=("    10=Khaman           -----       35.00 ")
+q=("    11=Panjabi Thali    -----       90.00 ")
+r=("    12=Paubhaji         -----       30.00 ")
+s=("    13=Dabelee          -----       30.00 ")
+t=("    14=patra            -----       30.00 ")
+u=("    15=chaa Thepala     -----       75.00 ")
+v=("    16=Pizza            -----      110.00 ")
+w=("    17=Chhas No Glash   -----       20.00 ")
+x=("    18=Day Ni Vataki    -----       25.00 ")
+
+A=("                                          ")
+B=("-----------Ice cream and cold drink------------")
+C=("                                          ")
+D=("    19=Strobery Con     -----       25.00 ")
+E=("    20=Faluda Con       -----       28.00 ")
+F=("    21=Family Ice Cream -----      200.00 ")
+G=("    22=Jumbo Ice Cream  -----      135.00 ")
+H=("    23=Mengo Candy      -----       25.00 ")
+I=("    24=Fourstick        -----       70.00 ")
+J=("    25=Coca Cola        -----       20.00 ")
+K=("    26=Thums Up         -----       20.00 ")
+L=("    27=Sprite           -----       20.00 ")
+M=("    28=POLO             -----       30.00 ")
+N=("    29=Fizz             -----       20.00 ")
+O=("    30=Red Bull         -----       80.00 ")
+
+
+total_bill=0
+k=[]
+person=int(input("\nHow many persons:"))
+ar=int(input("How many item you want:"))
+print("Please enter selected items numbers:\n")
+list1=[]
+for i in range(0,ar):
+    br=int(input())
+    list1.append
+
+#for i in list1:
+#    print("How many dishes for",i,"'s i
+
+#print(k)
+#print("items=",list1)
+P=("\n\n")    
+
+Q=("             RK FOOD ZONE       ")
+
+R=("===================================")
+S=("\n")
+T=("Item Description             Amount")
+U=("")
+V=("-----------------------------------")
+for i in list1:3
+    if(i==1):
+        total_bill=total_bill+(70)
+        print("1=Gathiya Jalebi   -----      70.00  ")
+    elif(i==2):
+        total_bill=total_bill+(30)
+        print("2=Masala Dosa      -----      30.00  ")
+    elif(i==3):
+        total_bill=total_bill+20
+        print("3=Plain Dosa       -----      20.00 ")
+    elif(i==4):
+        total_bill=total_bill+20
+        print("4=Papper Dosa      -----      20.00 ")
+    elif(i==5):
+        total_bill=total_bill+40
+        print("5=Edali Sanbhar    -----      40.00 ")
+    elif(i==6):
+        total_bill=total_bill+25
+        print("6=Tea              -----      25.00 ")
+    elif(i==7):
+        total_bill=total_bill+45
+        print("7=Coffee           -----      45.00 ")
+    elif(i==8):
+        total_bill=total_bill+120
+        print("8=Gujrati Thali    -----      120.00 ")
+    elif(i==9):
+        total_bill=total_bill+80
+        print("9=Bhajiya          -----      80.00 ")
+    elif(i==10):
+        total_bill=total_bill+35
+        print("10=Khaman           -----       35.00 ")
+    elif(i==11):
+        total_bill=total_bill+90
+        print("11=Panjabi Thali    -----       90.00 ")
+    elif(i==12):
+        total_bill=total_bill+30
+        print("12=Paubhaji         -----       30.00 ")
+    elif(i==13):
+        total_bill=total_bill+30
+        print("13=Dabelee          -----       30.00 ")
+    elif(i==14):
+        total_bill=total_bill+30
+        print("14=patra            -----       30.00  ")
+    elif(i==15):
+        total_bill=total_bill+75
+        print("15=chaa Thepala     -----       75.00 ")
+    elif(i==16):
+        total_bill=total_bill+110
+        print("16=Pizza            -----      110.00 ")
+    elif(i==17):
+        total_bill=total_bill+20
+        print("17=Chhas No Glash   -----       20.00  ")
+    elif(i==18):
+        total_bill=total_bill+20
+        print("17=Chhas No Glash   -----       20.00  ")
+    elif(i==19):
+        total_bill=total_bill+25
+        print("19=Strobery Con     -----       25.00 ")
+    elif(i==20):
+        total_bill=total_bill+28
+        print("20=Faluda Con       -----       28.00 ")
+    elif(i==21):
+        total_bill=total_bill+200
+        print("21=Family Ice Cream -----      200.00 ")
+    elif(i==22):
+        total_bill=total_bill+135
+        print("22=Jumbo Ice Cream  -----      135.00 ")
+    elif(i==23):
+        total_bill=total_bill+25
+        print("23=Mengo Candy      -----       25.00 ")
+    elif(i==24):
+        total_bill=total_bill+70
+        print("24=Fourstick        -----       70.00 ")
+    elif(i==25):
+        total_bill=total_bill+20
+        print("25=Coca Cola        -----       20.00 ")
+    elif(i==26):
+        total_bill=total_bill+20
+        print("26=Thums Up         -----       20.00 ")
+    elif(i==27):
+        total_bill=total_bill+20
+        print("27=Sprite           -----       20.00 ")
+    elif(i==28):
+        total_bill=total_bill+30
+        print("28=POLO             -----       30.00 ")
+    elif(i==29):
+        total_bill=total_bill+20
+        print("29=Fizz             -----       20.00 ")
+    elif(i==30):
+        total_bill=total_bill+80
+        print("30=Red Bull         -----       80.00 ")
+    
+W=("\n")
+X=(f"TOTAL BILL         -----     {total_bill}")
+
+
+from fpdf import FPDF
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font("Arial", size = 15)
+pdf.cell(200, 10, txt =a,
+            ln = 1, align = 'C')
+pdf.cell(200, 10, txt=b,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=c,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=d,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=e,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=f,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=g,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=h,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=ins,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=j,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=kin,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=l,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=m,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=n,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=o,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=p,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=q,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=r,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=s,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=t,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=u,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=v,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=w,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=x,
+            ln = 2, align = 'C')
+
+
+pdf.cell(200, 10, txt=A,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=B,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=C,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=D,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=E,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=F,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=G,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=H,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=I,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=J,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=K,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=L,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=M,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=N,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=O,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=P,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=Q,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=R,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=S,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=T,
+            ln = 2, align = 'C')
+
+pdf.cell(200, 10, txt=V,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, txt=W,
+            ln = 2, align = 'C')
+pdf.cell(200, 10, 10, txt=total_bill,)
+#             ln = 2, align = 'C')
+pdf.output("GFG.pdf")
